e11755990a29a2cb8c75364b856ad42fe6115b61
Taisei Nakajima <ne300085@senshu-u.jp>
2021-09-23 00:15:20 +0900

    add some structs

--- src/extension.rs
+++ src/extension.rs
@@ -1,6 +1,37 @@
 use wasmtime::*;
 use wasmtime_wasi::sync::WasiCtxBuilder;
 
+pub struct Extensions<T> {
+    pub engine: Engine,
+    pub extensions: Vec<Extension<T>>,
+}
+
+pub struct Extension<T> {
+    pub store: Store<T>,
+    pub module: Module,
+    pub linker: Linker<T>,
+}
+
+impl Extension<wasmtime_wasi::WasiCtx> {
+    pub fn default(engine: Engine, name: &str) -> Self {
+        let module =
+            Module::from_file(&engine, format!("./extension/src/wasm/{}.wasm", name)).unwrap();
+
+        let mut linker = Linker::new(&engine);
+        // wasi setup
+        wasmtime_wasi::add_to_linker(&mut linker, |s| s).unwrap();
+        let wasi = WasiCtxBuilder::new().inherit_stdio().build();
+
+        let mut store = Store::new(&engine, wasi);
+
+        Extension {
+            store,
+            module,
+            linker,
+        }
+    }
+}
+
 pub fn run() {
     let engine = Engine::default();
     let module = Module::from_file(&engine, "./extension/src/wasm/extension.wasm").unwrap();
@@ -18,4 +49,10 @@ pub fn run() {
         .unwrap()
         .call(&mut store, ())
         .unwrap();
+
+    // let instance = linker.instantiate(&mut store, &module).unwrap();
+    // let func = instance
+    //     .get_typed_func::<(), (), _>(&mut store, "main")
+    //     .unwrap();
+    // func.call(&mut store, ()).unwrap();
 }
